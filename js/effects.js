729be52f1fee58fffaa124bd006ffa9e5e6bddd4
SaintSGS-hub <neuroniosgames@gmail.com>
2026-01-12 00:51:03 -0300

    Update effects.js

--- js/effects.js
+++ js/effects.js
@@ -181,9 +181,9 @@ if (window.matchMedia("(max-width: 640px)").matches) {
   disclaimer.style.pointerEvents = 'none';
   disclaimer.style.lineHeight = '1.2em';
   disclaimer.innerHTML = `
-    2026©
     Este site não é oficial do <strong>Where Winds Meet</strong>. Serviço de fã e revenda de Echo Beads.<br>
     Música de fundo: "An Addictive Hit Song :Way of the Sword" por Liu Siyuan / RICORAY / Niko Cat - Em todas as plataformas digitais.
+    2026© Dados técnicos básicos podem ser coletados para fins de segurança e prevenção a fraudes 2026©
   `;
   document.body.appendChild(disclaimer);
 })();
